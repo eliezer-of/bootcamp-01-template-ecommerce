5b56754af48010c610b20ad60636363d952e14f1
Eliézer Fernandes <eliezer.oliveirasiueg@gmail.com>
2020-10-19 13:58:03 -0300

    removendo imports desnecessários

--- src/main/java/com/br/ecommerce/controller/AuthenticationController.java
+++ src/main/java/com/br/ecommerce/controller/AuthenticationController.java
@@ -1,7 +1,5 @@
 package com.br.ecommerce.controller;
 
-import com.br.ecommerce.model.Categoria;
-import com.br.ecommerce.requests.CategoriaRequest;
 import com.br.ecommerce.security.JwtUtil;
 import com.br.ecommerce.security.LoginInputDto;
 import org.slf4j.Logger;
@@ -16,11 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/api/auth")
