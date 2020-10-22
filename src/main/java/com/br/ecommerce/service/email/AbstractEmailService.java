29177445106a5e2e56ae3aa2810f19fe9199f777
Eliézer Fernandes <eliezer.oliveirasiueg@gmail.com>
2020-10-22 13:28:40 -0300

    adicionando link do produto no corpo do email - ps: no email da pergunta realizada

--- src/main/java/com/br/ecommerce/service/email/AbstractEmailService.java
+++ src/main/java/com/br/ecommerce/service/email/AbstractEmailService.java
@@ -4,23 +4,25 @@ import com.br.ecommerce.model.Pergunta;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 
+import java.net.URI;
+
 public abstract class AbstractEmailService implements EmailService {
 
     @Value("${default.sender}")
     private String sender;
 
     @Override
-    public void enviarEmailNovaPergunta(Pergunta pergunta) {
-        SimpleMailMessage sm = preparNovaPerguntaEmail(pergunta);
+    public void enviarEmailNovaPergunta(Pergunta pergunta, URI link) {
+        SimpleMailMessage sm = preparNovaPerguntaEmail(pergunta, link);
         enviarEmail(sm);
     }
 
-    protected SimpleMailMessage preparNovaPerguntaEmail(Pergunta pergunta) {
+    protected SimpleMailMessage preparNovaPerguntaEmail(Pergunta pergunta,URI link) {
         SimpleMailMessage sm = new SimpleMailMessage();
         sm.setTo(pergunta.getDonoProduto().getEmail());
         sm.setFrom(sender);
         sm.setSubject("Nova pergunta para o produto id: "+ pergunta.getProduto().getId() + " - " + pergunta.getProduto().getNome());
-        sm.setText(pergunta.getTitulo());
+        sm.setText(pergunta.getTitulo() + " - Visualizar o produto: " + link);
         return sm;
     }
 }
--- src/main/java/com/br/ecommerce/service/email/EmailService.java
+++ src/main/java/com/br/ecommerce/service/email/EmailService.java
@@ -3,9 +3,11 @@ package com.br.ecommerce.service.email;
 import com.br.ecommerce.model.Pergunta;
 import org.springframework.mail.SimpleMailMessage;
 
+import java.net.URI;
+
 public interface EmailService {
 
     void enviarEmail(SimpleMailMessage mensagem);
 
-    void enviarEmailNovaPergunta(Pergunta pergunta);
+    void enviarEmailNovaPergunta(Pergunta pergunta, URI link);
 }
